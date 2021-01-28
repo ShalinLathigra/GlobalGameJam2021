4701ae7b0dcbbcd5a5cd8f1eb5eb47f411d5392b
Shalin Lathigra <shalinlathigra@gmail.com>
2021-01-28 14:06:15 -0500

    Remove Globals.gd, add tool for swapping out levels.
    Signed-off-by: Shalin Lathigra <shalinlathigra@gmail.com>

--- Scripts/MapController.gd
+++ Scripts/MapController.gd
@@ -1,15 +1,33 @@
 extends Node2D
 
 const maps = [
-	preload("res://Scenes/Testing/Map_01.tscn")
+	preload("res://Scenes/Testing/Map_01.tscn"),
+	preload("res://Scenes/Testing/Map_02.tscn")
 	]
+	
+enum map_indices {MORNING_01, MORNING_02}
+export (map_indices) var i
+	
 export (NodePath) var cam;
-export (NodePath) var test_map;
+export (NodePath) var map_path;
 
 func _ready():
-	var map_i = maps[0].instance()
-	add_child(map_i)
+	_update_map()
+	
+func _update_map():
+	if (get_node(map_path)):
+		get_node(map_path).queue_free()
+	
+	var new_map = maps[i].instance()
+	add_child(new_map)
+	map_path = new_map.name
 	
 	if (get_node(cam)):
-		get_node(cam).set_walls(map_i)
-	get_node(test_map).queue_free()
+		get_node(cam).set_walls(new_map)
+		
+	
+func _input(event):
+	if (Input.is_action_just_pressed("next_map")):
+		i += 1
+		i %= map_indices.size()
+		_update_map()
--- Scripts/Shop.gd
+++ Scripts/Shop.gd
@@ -1,11 +1,20 @@
 extends Area2D
 class_name Shop
-export (Globals.item_type) var type;
-var item = Globals.item_map[type]
+
+
+onready var image_map = {
+	item_type.FLOWER: preload("res://Imports/Items/Flowers.png")
+}
+onready var item_map = {
+	item_type.FLOWER: preload("res://Scenes/Components/Item.tscn")
+}
+
+enum item_type {FLOWER}
+
+export (item_type) var type
 
 func _ready():
-	$Sprite.texture = Globals.image_map[type]
-	item = Globals.item_map[type].instance()
+	$Sprite.texture = image_map[type]
 
 func _on_Shop_body_entered(body):
-	body.set_item(item)
+	body.set_item(item_map[type].instance())
